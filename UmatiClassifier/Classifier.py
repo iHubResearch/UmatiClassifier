bbd1dcfc0946c25b8c14bbfd560e5cb5b15b5b5f
GBelzoni <patrickhcostello@gmail.com>
2013-11-10 20:54:53 +0300

    First Classifier working

--- UmatiClassifier/Classifier.py
+++ UmatiClassifier/Classifier.py
@@ -21,14 +21,12 @@ rawdata = xls.parse('20131107_UmatiData', index_col=None, na_values=['NA'])
 rawdata = rawdata.dropna(subset=['Actual text'])
 
 # Extract features
-text = rawdata['Actual text'] 
-floats = []
-for id, it in enumerate(text.tolist()):
-    if isinstance(it, float):
-        floats.append(id)
-floats
-
-unicode(text[9])
+text = rawdata['Actual text']
+# floats = []
+# for id, it in enumerate(text.tolist()):
+#     if isinstance(it, float):
+#         floats.append(id)
+# floats
 
 
 #Make large word features
@@ -37,13 +35,64 @@ unicode(text[9])
 from sklearn.feature_extraction.text import CountVectorizer
 vectoriser_training = CountVectorizer(min_df=1,stop_words='english',strip_accents='unicode')
 t = time.time()
-features_msg_training = vectoriser_training.fit_transform(text) 
+features = vectoriser_training.fit_transform(text) 
 print "training text to word vector took", time.time()-t, "seconds"
-features_msg_training.shape
+features.shape
 #Change from sparse matrix to dense matrix
 #vect_train = features_msg_training.todense() #BIG memory usage 2GB, see how to use the sparse for training
 
+#Create target
+target = rawdata['The text /article can be seen as encouraging the audience to']
+target = target.values
+target.shape
 
-# Create classifiers
+#Create classifier
+from sklearn.svm import LinearSVC
+X= features
+y = target
 
-# Use n-fold validation to check which is best.
+t = time.time()
+clf=LinearSVC(C=1,penalty ='l1',dual=False).fit(X,y)
+print "training took", time.time()-t, "seconds"
+
+#Check performance
+training_predicted = clf.predict(X)
+
+training_predicted
+
+from sklearn import metrics
+cm_training = metrics.confusion_matrix(y,training_predicted)
+print "Confusion Matrix on training data"
+print cm_training
+
+from sklearn import cross_validation
+y
+Xcsr = X.tocsr()
+row = Xcsr[0].todense()
+skf = cross_validation.StratifiedKFold(y=y, n_folds=3)
+for train_index, test_index in skf:
+    print("TRAIN:", train_index, "TEST:", test_index)
+    X_train, X_test = Xcsr[train_index], Xcsr[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+
+
+#Cross validation not straightforward with sparse matrix
+#as iterating over k-folds requires X to dense as has to slice
+#on ranges. When doing the fitting this kill computer memory
+
+clfs = []
+for train_index, test_index in skf:
+    clfs.append(clf.fit(Xcsr[train_index],y[train_index]))
+#      print clf.fit(Xcsr[train_index],y[train_index]).score(Xcsr[test_index],y[test_index])
+
+
+this_predicted = clfs[1].predict(X)
+cm_training = metrics.confusion_matrix(y,this_predicted)
+print "Confusion Matrix on training data"
+print cm_training.shape
+
+
+
+df=pd.DataFrame(target)
+df['predicted']=this_predicted
+df.to_csv('output.csv')
