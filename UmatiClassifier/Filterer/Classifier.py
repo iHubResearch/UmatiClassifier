0182b7eab460e1648ad1e9595f2cab25f245c26b
GBelzoni <patrickhcostello@gmail.com>
2014-01-15 22:24:23 +0300

    Rearranging folders

--- UmatiClassifier/Filterer/Classifier.py
+++ UmatiClassifier/Filterer/Classifier.py
@@ -8,9 +8,18 @@ Created on Nov 10, 2013
 #     pass
 from BoilerPlate import *
 
+## get unique labels as done in sklearn.metrics super annoytng
+def unique_labels(*lists_of_labels):
+    """Extract an ordered array of unique labels"""
+    labels = set().union(*(l.ravel() if hasattr(l, "ravel") else l for l in lists_of_labels))
+    return np.asarray(sorted(labels))
+
+
+
+
+
 # Import data
 rawdata = pd.read_csv('20131107_UmatiData.csv')
-rawdata = pd.read_csv('20131107_UmatiData.txt',sep='\t')
 
 from pandas.io.parsers import  ExcelFile
 xls = ExcelFile('20131107_UmatiData.xls')
@@ -46,13 +55,39 @@ target = rawdata['The text /article can be seen as encouraging the audience to']
 target = target.values
 target.shape
 
+#Create multilable target
+target_ml = [it.replace('animals, ','animals,') for it in target] #Do this so don't split category incorrectly
+target_ml = [ it.split(', ') for it in target_ml]
+target_ml = [ [it.strip() for it in row] for row in target_ml ]
+target_ml[:30]
+[it for it in target_ml if len(it)>1]
+
+
+from sklearn.preprocessing import LabelBinarizer
+
+lb = LabelBinarizer()
+lb.fit(target_ml)
+lb.classes_
+lb.multilabel
+bin_labels = lb.transform(target_ml)
+bin_labels.shape
+# binLabels = [lb.transform(it) for it in target_ml]
+binLabels[0]
+
+type((1,2,3))
+
 #Create classifier
 from sklearn.svm import LinearSVC
+from sklearn.multiclass import OneVsRestClassifier
 X= features
-y = target
+y = target_ml
 
 t = time.time()
-clf=LinearSVC(C=1,penalty ='l1',dual=False).fit(X,y)
+clfinner=LinearSVC(C=1,penalty ='l1',dual=False)
+
+clf = OneVsRestClassifier(clfinner)
+clf.fit(X,y)
+clf.multilabel_
 print "training took", time.time()-t, "seconds"
 
 #Check performance
@@ -61,19 +96,25 @@ training_predicted = clf.predict(X)
 training_predicted
 
 from sklearn import metrics
+
 cm_training = metrics.confusion_matrix(y,training_predicted)
 print "Confusion Matrix on training data"
 print cm_training
 
 from sklearn import cross_validation
-y
+
+y[(1,4)]
+
+[ y[it] for it in [1,4] ]
+
 Xcsr = X.tocsr()
 row = Xcsr[0].todense()
-skf = cross_validation.StratifiedKFold(y=y, n_folds=3)
+skf = cross_validation.StratifiedKFold(y=target, n_folds=3)
 for train_index, test_index in skf:
     print("TRAIN:", train_index, "TEST:", test_index)
     X_train, X_test = Xcsr[train_index], Xcsr[test_index]
-    y_train, y_test = y[train_index], y[test_index]
+    y_train, y_test = [y[it] for it in train_index], [y[it] for it in test_index]
+    
 
 
 #Cross validation not straightforward with sparse matrix
@@ -81,10 +122,40 @@ for train_index, test_index in skf:
 #on ranges. When doing the fitting this kill computer memory
 
 clfs = []
+cms = []
+labels = []
 for train_index, test_index in skf:
-    clfs.append(clf.fit(Xcsr[train_index],y[train_index]))
-#      print clf.fit(Xcsr[train_index],y[train_index]).score(Xcsr[test_index],y[test_index])
+    y_train = [y[it] for it in train_index]
+    y_test = [y[it] for it in test_index]
+    this_clf =clf.fit(Xcsr[train_index],y_train)
+    clfs.append(this_clf)
+    this_predicted = this_clf.predict(Xcsr[test_index])
+    print np.array(zip(y_test,this_predicted))
+    
+# #         print clf.fit(Xcsr[train_index],y[train_index]).score(Xcsr[test_index],y[test_index])
+# #     this_label1 = unique_labels(list(y[test_index]))
+# #     this_label2 = unique_labels(list(this_predicted))
+#     this_label1 = np.unique(y[test_index]).tolist()
+#     this_label2 = np.unique(this_predicted).tolist()
+#     this_label = list(set(this_label1).union( set(this_label2)))
+#     print this_label
+# #     this_label = this_label.tolist()
+#     print len(this_label)
+#     cm_test = metrics.confusion_matrix(y_test,
+#                                        this_predicted)#,this_label) 
+#     print cm_test.shape
+# #     labels.append(this_label)
+#     cms.append(cm_test)
+#     
+#    
 
+
+ 
+df = pd.DataFrame(cms[0], index= labels[0], columns=labels[0])
+
+cms[0]
+
+df.to_csv('output.csv')
 
 this_predicted = clfs[1].predict(X)
 cm_training = metrics.confusion_matrix(y,this_predicted)
